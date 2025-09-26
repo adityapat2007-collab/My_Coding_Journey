7154afbaa1073c3ec09a374e4941bfce4312e70c
Aditya <adityapat2007@gmail.com>
2025-09-26 18:21:12 +0530

    Moore's Voting Algorithm

---
+++ moore's_voting.py
@@ -0,0 +1,25 @@
+# to find: majority element (occurs more than n/2 times, n=len(arr))
+#brute force: nested for loops
+#better: hash array
+#optimal: Moore's voting algorithm
+
+arr = [7,7,5,7,5,1,1,5,7,2,3,5,5,7,7,5,5,5,5]
+#first we assume starting element to be the majority
+maj = arr[0]
+count = 1
+for i in range(1, len(arr)):
+    if arr[i] == maj:
+        count+=1
+    else:
+        count-=1
+    if count == 0:
+        maj = arr[i]
+# now, the element stored in 'maj' may or may not be the majority element, so verify:
+count = 1
+for i in range(1, len(arr)):
+    if arr[i] == maj:
+        count+=1
+if count > len(arr)/2:
+    print(maj)
+else:
+    print("No majority element")
